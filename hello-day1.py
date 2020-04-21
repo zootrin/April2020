01adc15b080cca129ef2023ce18759fa980592b8
Alasdair Rothnie <alasdair.rothnie@gmail.com>
2020-04-20 17:29:06 -0700

    Day 1 Python

---
+++ hello-day1.py
@@ -0,0 +1,21 @@
+print("Hello World")
+print(2+2)
+print("2+2")
+a=2
+b=2
+print(a)
+print(b)
+print(a+b)
+name="AI"
+print(name)
+surname="Machine"
+print(surname)
+print(name+surname)
+print(name, surname)
+print(name+" "+surname)
+
+a="Python"
+b="is"
+c="cool!"
+
+print(a, b, c)
